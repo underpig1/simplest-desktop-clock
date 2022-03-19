660297ac199a2263a39be92d388067500f3dd287
Underpig <43320976+underpig1@users.noreply.github.com>
2022-03-18 23:23:40 -1000

    Create solid_black.bat

---
+++ themes/solid_black.bat
@@ -0,0 +1,6 @@
+@echo off
+echo COLOR_SHIFT = (0, 0, 00) > "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\custom.py"
+echo POSITION = (600, 75) >> "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\custom.py"
+echo FONT = ("Ariel", 75, "bold italic") >> "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\custom.py"
+echo INVERSE = 0 >> "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\custom.py"
+echo SOLID = 1 >> "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\custom.py"
