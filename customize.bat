61a50c5036a6de56001a7c0229a792e8294287ac
Underpig <43320976+underpig1@users.noreply.github.com>
2022-03-18 21:26:44 -1000

    Update customize.bat

--- customize.bat
+++ customize.bat
@@ -3,6 +3,6 @@ set /p r="Enter RED value of RGB shift from wallpaper color (value can be negati
 set /p g="Enter GREEN value of RGB shift from wallpaper color (value can be negative): "
 set /p b="Enter BLUE value of RGB shift from wallpaper color (value can be negative): "
 set /p x="Enter X value of clock position relative to the top right corner of the screen: "
-set /p y="Enter X value of clock position relative to the top right corner of the screen: "
+set /p y="Enter Y value of clock position relative to the top right corner of the screen: "
 echo COLOR_SHIFT = (%r%, %g%, %b%) > custom.py
 echo POSITION = (%x%, %y%) >> custom.py
