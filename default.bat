63be1418840d9562b8ab7be61c6641a0d6723c52
Underpig <43320976+underpig1@users.noreply.github.com>
2022-03-18 21:40:22 -1000

    Create default.bat

---
+++ default.bat
@@ -0,0 +1,4 @@
+@echo off
+echo COLOR_SHIFT = (-50, -50, -50) > "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\custom.py"
+echo POSITION = (600, 75) >> "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\custom.py"
+echo FONT = ("Roboto Mono", 75, "bold") >> "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\custom.py"
