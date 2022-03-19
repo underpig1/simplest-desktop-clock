efccc5f6a21057bdad9d69ebc586582a58815f29
Underpig <43320976+underpig1@users.noreply.github.com>
2022-03-19 10:54:03 -1000

    Create roboto.bat

---
+++ themes/roboto.bat
@@ -0,0 +1,6 @@
+@echo off
+echo COLOR_SHIFT = (-50, -50, -50) > "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\custom.py"
+echo POSITION = (600, 75) >> "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\custom.py"
+echo FONT = ("Roboto Mono", 75, "bold") >> "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\custom.py"
+echo INVERSE = 0 >> "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\custom.py"
+echo SOLID = 0 >> "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\custom.py"
