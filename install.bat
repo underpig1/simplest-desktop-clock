fb30577c67e8e2868692009b41b2832ebc285b6c
Underpig <43320976+underpig1@users.noreply.github.com>
2022-03-18 21:44:00 -1000

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,3 +1,4 @@
 pip install -r requirements.txt
 xcopy clock.pyw "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\"
 xcopy custom.py "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\"
+"%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\clock.pyw"
