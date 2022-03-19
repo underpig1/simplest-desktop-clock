30381de2aa87c8999c0b5a7017164d042696baa6
Underpig <43320976+underpig1@users.noreply.github.com>
2022-03-18 23:20:12 -1000

    Update defaults.bat

--- themes/defaults.bat
+++ themes/defaults.bat
@@ -3,3 +3,4 @@ echo COLOR_SHIFT = (-50, -50, -50) > "%appdata%\Microsoft\Windows\Start Menu\Pro
 echo POSITION = (600, 75) >> "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\custom.py"
 echo FONT = ("Roboto Mono", 75, "bold") >> "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\custom.py"
 echo INVERSE = 0 >> "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\custom.py"
+echo SOLID = 0 >> "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\custom.py"
